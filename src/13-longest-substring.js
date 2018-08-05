b8c6292e8bcea887550d55aac5924298fa11ee0c
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2018-08-06 01:45:20 +0300

    Add faster solution for problem 13

--- src/13-longest-substring.js
+++ src/13-longest-substring.js
@@ -3,7 +3,7 @@
 // Given an integer k and a string s, find the length of the longest substring
 // that contains at most k distinct characters.
 
-function longestSubstring (string, k) {
+function longestSubstringSlow (string, k) { // eslint-disable-line no-unused-vars
   let bounds = [0, 0]
   for (let i = 0; i < string.length; i++) {
     let set = new Set()
@@ -24,4 +24,40 @@ function longestSubstring (string, k) {
   return string.slice(bounds[0], bounds[1])
 }
 
-module.exports = longestSubstring
+function longestSubstringFast (string, k) {
+  let bounds = [0, 0]
+  let start = 0
+  let occurrences = new Map()
+
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i]
+    let currentOcc = occurrences.get(char) || 0
+
+    occurrences.set(char, currentOcc + 1)
+
+    while (occurrences.size > k) {
+      let len = i - start
+      if (len > bounds[1] - bounds[0]) {
+        bounds = [start, i]
+      }
+
+      char = string[start++]
+      currentOcc = occurrences.get(char)
+
+      if (currentOcc === 1) occurrences.delete(char)
+      else occurrences.set(char, currentOcc - 1)
+    }
+  }
+
+  if (occurrences.size <= k) {
+    let len = string.length - start
+    if (len > bounds[1] - bounds[0]) {
+      bounds = [start, string.length]
+    }
+  }
+
+  return string.slice(bounds[0], bounds[1])
+}
+
+module.exports = longestSubstringFast
+longestSubstringFast('abcba', 2)
